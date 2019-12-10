01fa0c9d602c65209718cd4eee69fd029aaed250
theneuralbeing <bhavesh.laddagiri1@gmail.com>
2019-12-11 01:47:00 +0530

    first commit

---
+++ web_app/precache-manifest.3dfc8445b83fe338fc5cd4151ecbbee5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38eeb7d2837ff8a2a05b010714e3694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fdec0a67c7dd817c63e",
+    "url": "/static/js/2.fcafb935.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fcafb935.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91a1dddfc759ad969672",
+    "url": "/static/js/main.57c01656.chunk.js"
+  },
+  {
+    "revision": "d2199729f0a20f3048ca",
+    "url": "/static/js/runtime-main.06f8c5bc.js"
+  },
+  {
+    "revision": "ca4db02faadb80183e5923407be1cf82",
+    "url": "/static/media/negative.ca4db02f.jpg"
+  },
+  {
+    "revision": "a1eebbce92f9473b92cdbbe098a51e27",
+    "url": "/static/media/positive.a1eebbce.jpg"
+  },
+  {
+    "revision": "4296b70dd4459b34060b859e42e32b9a",
+    "url": "/static/media/title.4296b70d.jpg"
+  }
+]);
